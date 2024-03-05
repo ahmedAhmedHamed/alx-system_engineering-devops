38091985b21212f0dd5a3d5fd0cc3a15e05532a5
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-03-05 16:45:27 +0200

    0 - added check

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,5 +11,9 @@ def number_of_subscribers(subreddit):
     gets the number of subscribers of a subreddit
     """
     r = requests.get('https://www.reddit.com/r/{}/about.json'.format(subreddit))
-    json_data = json.loads(r.text)
+    try:
+        json_data = json.loads(r.text)
+    except json.decoder.JSONDecodeError:
+        return 0
     return json_data['data']['subscribers']
+
