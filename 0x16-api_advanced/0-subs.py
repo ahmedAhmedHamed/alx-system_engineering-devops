605f19855ec5c2a52aee0d210f2df497f0e8b9c2
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-03-05 16:48:12 +0200

    0 - added check for status codes

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,10 +10,15 @@ def number_of_subscribers(subreddit):
     """
     gets the number of subscribers of a subreddit
     """
-    r = requests.get('https://www.reddit.com/r/{}/about.json'.format(subreddit))
+    r = requests.get('https://www.reddit.com/r/{}/about.json'.format(subreddit),
+                      allow_redirects=False)
+
+    if r.status_code!= 200:
+        return 0
+
     try:
         json_data = json.loads(r.text)
     except json.decoder.JSONDecodeError:
         return 0
-    return json_data['data']['subscribers']
 
+    return json_data['data']['subscribers']
