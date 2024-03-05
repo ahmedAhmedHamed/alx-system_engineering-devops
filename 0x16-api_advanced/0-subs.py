fc00c0ed3c26baa413e421a7ca0c5fe33333723a
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-03-05 16:57:54 +0200

    0 - pycodestyle

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,15 +10,16 @@ def number_of_subscribers(subreddit):
     """
     gets the number of subscribers of a subreddit
     """
-    r = requests.get('https://www.reddit.com/r/{}/about.json'.format(subreddit),
-                      allow_redirects=False)
+    headers = {'User-Agent': 'hamed_useragent'}
+    request_str = f'https://www.reddit.com/r/{subreddit}/about.json'
+    r = requests.get(
+        request_str,
+        allow_redirects=False,
+        headers=headers,
+        timeout=1000
+        )
 
-    if r.status_code!= 200:
+    if r.status_code != 200:
         return 0
-
-    try:
-        json_data = json.loads(r.text)
-    except json.decoder.JSONDecodeError:
-        return 0
-
+    json_data = json.loads(r.text)
     return json_data['data']['subscribers']
