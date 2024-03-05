079ed2432acc3804601aa120f4117cbe56621b3c
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-03-05 19:16:40 +0200

    1 - pycodestyle

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -17,11 +17,11 @@ def top_ten(subreddit):
         "limit": 8,
     }
     response = requests.get(request_str,
-                    headers=headers,
-                    allow_redirects=False,
-                    params=payload,
-                    timeout=1000
-                    )
+                            headers=headers,
+                            allow_redirects=False,
+                            params=payload,
+                            timeout=1000
+                            )
     if response.status_code != 200:
         print('None')
         return
