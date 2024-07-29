5af612c8fa7ab4dd71ebc72e67a2d1d323c4db43
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-07-29 14:48:35 +0300

    requirements 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -9,17 +9,23 @@ if __name__ == "__main__":
     """
     talk to this using command line arguments
     """
-    users = json.loads(urllib.request.urlopen(f"https://jsonplaceholder.typicode.com/users").read())
+    users =\
+        (json.
+         loads(urllib.request.urlopen(
+            f"https://jsonplaceholder.typicode.com/users").read()))
     username = None
     exported_json = {}
     for user in users:
         exportable_arr = []
         username = user["username"]
         user_id = user["id"]
-        query_string = f"https://jsonplaceholder.typicode.com/todos?userId={user_id}"
+        query_string =\
+            f"https://jsonplaceholder.typicode.com/todos?userId={user_id}"
         todos = json.loads(urllib.request.urlopen(query_string).read())
         for task in todos:
-            exportable_arr.append({"task": task["title"], "completed": task["completed"], "username": username})
+            exportable_arr.append({"task": task["title"],
+                                   "completed": task["completed"],
+                                   "username": username})
         exported_json[user_id] = exportable_arr
     with open(f"{'todo_all_employees'}.json", "w") as outfile:
         json.dump(exported_json, outfile)
