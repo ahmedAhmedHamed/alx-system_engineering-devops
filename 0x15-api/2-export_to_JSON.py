c288004055be9f52d2ef1df29f980ecafa627d74
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-07-29 14:47:04 +0300

    requirements 2

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -2,9 +2,9 @@
 """
 this gets tasks from API and converts them to CSV
 """
+import json
 import sys
 import urllib.request
-import json
 
 if __name__ == "__main__":
     """
@@ -12,8 +12,10 @@ if __name__ == "__main__":
     """
     emp_id = sys.argv[1]
     query = f"?userId={emp_id}"
-    todos = json.loads(urllib.request.urlopen(f"https://jsonplaceholder.typicode.com/todos{query}").read())
-    users = json.loads(urllib.request.urlopen(f"https://jsonplaceholder.typicode.com/users{query}").read())
+    todo_url = f"https://jsonplaceholder.typicode.com/todos{query}"
+    users_url = f"https://jsonplaceholder.typicode.com/users{query}"
+    todos = json.loads(urllib.request.urlopen(todo_url).read())
+    users = json.loads(urllib.request.urlopen(users_url).read())
     username = None
     exportable_arr = []
     for user in users:
@@ -22,7 +24,9 @@ if __name__ == "__main__":
             break
 
     for task in todos:
-        exportable_arr.append({"task": task["title"], "completed": task["completed"], "username": username})
+        exportable_arr.append({"task": task["title"],
+                               "completed": task["completed"],
+                               "username": username})
     exported_json = {emp_id: exportable_arr}
     with open(f"{emp_id}.json", "w") as outfile:
         json.dump(exported_json, outfile)
