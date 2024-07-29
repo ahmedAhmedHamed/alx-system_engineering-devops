54e1312c35c3ec1506499aca83d369ab1b49ab88
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-07-29 14:46:03 +0300

    requirements 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -2,10 +2,10 @@
 """
 this gets tasks from API and converts them to CSV
 """
+import csv
+import json
 import sys
 import urllib.request
-import json
-import csv
 
 if __name__ == "__main__":
     """
@@ -13,8 +13,10 @@ if __name__ == "__main__":
     """
     emp_id = sys.argv[1]
     query = f"?userId={emp_id}"
-    todos = json.loads(urllib.request.urlopen(f"https://jsonplaceholder.typicode.com/todos{query}").read())
-    users = json.loads(urllib.request.urlopen(f"https://jsonplaceholder.typicode.com/users{query}").read())
+    todo_url = f"https://jsonplaceholder.typicode.com/todos{query}"
+    users_url = f"https://jsonplaceholder.typicode.com/users{query}"
+    todos = json.loads(urllib.request.urlopen(todo_url).read())
+    users = json.loads(urllib.request.urlopen(users_url).read())
     username = None
     exportable_arr = []
     for user in users:
@@ -23,7 +25,9 @@ if __name__ == "__main__":
             break
 
     for task in todos:
-        exportable_arr.append([emp_id, username, task["completed"], task["title"]])
+        exportable_arr.append([emp_id, username, task["completed"],
+                               task["title"]])
     with open(f'{emp_id}.csv', 'w', newline='') as csvfile:
-        writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        writer = csv.writer(csvfile, delimiter=',', quotechar='"',
+                            quoting=csv.QUOTE_ALL)
         writer.writerows(exportable_arr)
