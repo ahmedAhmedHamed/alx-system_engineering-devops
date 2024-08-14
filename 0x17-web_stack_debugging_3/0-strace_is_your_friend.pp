9ffb54126971097658abcae73b76666b7657413f
ahmed hamed <ahmedahmedhamedahmed0@gmail.com>
2024-08-14 14:59:16 +0300

    0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
-# Fixing a typo in file includes.
-# utilises sed to do so.
+# Ahmed Hamed - Fixing a typo in file includes.
+# utilises sed and puppet to do so.
 
 exec {'fix-typo':
   command => "/bin/sed -i /var/www/html/wp-settings.php -e 's/class-wp-locale.phpp/class-wp-locale.php/'"
